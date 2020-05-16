c4748d09eccfa42dd7c6669205d393becd7c6927
0322keisuke <isikeidesk@gmail.com>
2020-05-16 17:43:46 +0900

    create event form and event table with bootsrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
-    <div>
-      This is a template for React app.
-    </div>
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4> 
+      <form>
+        <div className="form group">
+          <label htmlFor="fromEventTitle">タイトル</label>
+          <input className="form-control" id="fromEventTitle" />
+        </div>
+
+        <div className="form group">
+          <label htmlFor="fromEventBody">ボディ</label>
+          <textarea className="form-control" id="fromEventBody" />
+        </div>
+
+        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">タイトル</th>
+            <th scope="col">ボディー</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+      </div>
   )
 }
 
