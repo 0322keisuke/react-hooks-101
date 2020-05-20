0adc7b43feefbf45355b8bae1e023313984b9ab8
0322keisuke <isikeidesk@gmail.com>
2020-05-20 21:24:39 +0900

    refactor events with reducer

---
+++ src/reducers/events.js
@@ -0,0 +1,64 @@
+import {
+    CREATE_EVENT,
+    DELETE_ALL_EVENTS,
+    DELETE_EVENT
+} from '../actions'
+//action = {
+//  type:'CREATE_EVENT'
+//  titile: '2020東京オリンピックのお知らせ'
+//  body:2020年に東京でオリンピックを開催します！つきましては、、、、'
+//}
+//
+//state = []   空配列時は何もしなくてＯＫ。
+//state = [
+//    id: 1, title: 'タイトル1, body:'ボディー１'},
+//    id: 2, title: 'タイトル2, body:'ボディー2'},
+//    id: 3, title: 'タイトル3, body:'ボディー3'},
+//]
+//
+///# before   初期リスト
+//state = []
+//# after
+//state = [
+// {
+//  id:1,
+//  titile: '2020東京オリンピックのお知らせ',
+//  body:2020年に東京でオリンピックを開催します！つきましては、、、、'
+// }
+//]
+//#before　既に他にリストがある場合
+///state = [
+//    {id: 1, title: 'タイトル1, body:'ボディー１'},
+//    {id: 2, title: 'タイトル2, body:'ボディー2'},
+//    {id: 3, title: 'タイトル3, body:'ボディー3'},
+//]
+//
+//#after
+///state = [
+//    {id: 1, title: 'タイトル1, body:'ボディー１'},
+//    {id: 2, title: 'タイトル2, body:'ボディー2'},
+//    {id: 3, title: 'タイトル3, body:'ボディー3'},
+//    {
+//     id:4,
+//     titile: '2020東京オリンピックのお知らせ',
+//     body:2020年に東京でオリンピックを開催します！つきましては、、、、'
+//    }
+//]
+
+const events = (state = [], action) => {
+ switch(action.type){
+    case CREATE_EVENT:
+        const event = {title: action.title, body: action.body }
+        const length = state.length
+        const id = length === 0 ? 1 : state[length - 1].id + 1
+        return [...state, {id: id, ...event}]
+    case DELETE_EVENT:
+        return state.filter(event => event.id !== action.id)
+    case DELETE_ALL_EVENTS:
+        return []
+    default:
+        return state
+ }
+}
+
+export default events
